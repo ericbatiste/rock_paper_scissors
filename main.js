fef4f652a3629c28ba6102a1baedcc4f752b9af1
Eric Batiste <ericbatiste4@gmail.com>
2023-11-11 10:44:19 -0700

    Allow user to select game type and navigate to appropriate select fighter display

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+var selectGameType = document.querySelector('.select-game-container');
+var classicGame = document.querySelector('#classicGame');
+var extendedGame = document.querySelector('#extendedGame');
+var selectClassicFighter = document.querySelector('#classicFighters');
+var selectExtendedFighter = document.querySelector('#extendedFighters');
+var chooseGameBtn = document.querySelector('#chooseGameBtn');
+var resetScoreBtn = document.querySelector('#resetScoreBtn');
+
+var userFighter;
+var compFighter;
+
 var classicfighters = [
     {name: 'Rock', defeats: ['Scissors']},
     {name: 'Scissors', defeats: ['Paper']},
@@ -12,8 +23,20 @@ var expandedFighters = [
     {name: 'Teddy', defeats: ['Alien', 'Shroom']}
 ]
 
-var userPick;
-var compPick;
+selectGameType.addEventListener('click', selectGame)
+
+function selectGame(e) {
+    var selectedGame = e.target.closest('article');
+    if (selectedGame.id === 'classicGame') {
+        toggleDisplay(selectGameType);
+        toggleDisplay(selectClassicFighter);
+        toggleDisplay(chooseGameBtn);
+    } else if (selectedGame.id === 'extendedGame') {
+        toggleDisplay(selectGameType);
+        toggleDisplay(selectExtendedFighter);
+        toggleDisplay(chooseGameBtn);
+    }
+}
 
 function updateComp(currentFighter, wins = 0,) {
     return {
@@ -37,12 +60,22 @@ function checkForVictory(user, comp) {
     if (user.currentFighter.defeats.includes(comp.currentFighter.name)) {
         user.victor = true;
         user.wins++;
+        console.log('user');
     } else if (comp.currentFighter.defeats.includes(user.currentFighter.name)) {
-        computer.victor = true;
-        computer.wins++
+        comp.victor = true;
+        comp.wins++
+        console.log('comp')
     }
 }
 
 function getRandomFighter(fighters) {
     return fighters[Math.floor(Math.random() * fighters.length)]
+}
+
+function toggleDisplay(element) {
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
 }
