62c3c0e9f1b2aea4382bb61b937fb62e57e8d721
Eric Batiste <ericbatiste4@gmail.com>
2023-11-13 16:23:51 -0700

    Disallow user to play game while user name input form is open.

--- data.js
+++ data.js
@@ -1,27 +1,59 @@
 var user = {
-    name: 'Human',
-    currentFighter: null,
-    wins: 0,
-    victor: false
-}
+  name: "Human",
+  currentFighter: null,
+  wins: 0,
+  victor: false,
+};
 
 var comp = {
-    name: 'Steve',
-    currentFighter: null,
-    wins: 0,
-    victor: false
-}
+  name: "Steve",
+  currentFighter: null,
+  wins: 0,
+  victor: false,
+};
 
 var classicFighters = [
-    {name: 'Rock', icon: 'assets/Rock.png', defeats: ['Scissors']},
-    {name: 'Scissors', icon: 'assets/Scissors.png', defeats: ['Paper']},
-    {name: 'Paper', icon: 'assets/Paper.png', defeats: ['Rock']},
-]
+  {
+    name: "Rock", 
+    icon: "assets/Rock.png", 
+    defeats: ["Scissors"]
+  },
+  {
+    name: "Scissors", 
+    icon: "assets/Scissors.png", 
+    defeats: ["Paper"]
+  },
+  {
+    name: "Paper",
+    icon: "assets/Paper.png", 
+    defeats: ["Rock"]
+  }
+];
 
 var extendedFighters = [
-    {name: 'Gemini', icon: 'assets/Gemini.png', defeats: ['Leo', 'Sagittarius']},
-    {name: 'Leo', icon: 'assets/Leo.png', defeats: ['Sagittarius', 'Scorpio']},
-    {name: 'Sagittarius', icon: 'assets/Sagittarius.png', defeats: ['Scorpio', 'Virgo']},
-    {name: 'Scorpio', icon: 'assets/Scorpio.png', defeats: ['Virgo', 'Gemini']},
-    {name: 'Virgo', icon: 'assets/Virgo.png', defeats: ['Gemini', 'Leo']}
-]
+  {
+    name: "Gemini",
+    icon: "assets/Gemini.png",
+    defeats: ["Leo", "Sagittarius"]
+  },
+  { 
+    name: "Leo", 
+    icon: "assets/Leo.png", 
+    defeats: ["Sagittarius", "Scorpio"]
+  },
+  {
+    name: "Sagittarius",
+    icon: "assets/Sagittarius.png",
+    defeats: ["Scorpio", "Virgo"]
+  },
+  { 
+    name: "Scorpio", 
+    icon: "assets/Scorpio.png", 
+    defeats: ["Virgo", "Gemini"] 
+  },
+  { 
+    name: "Virgo", 
+    icon: "assets/Virgo.png", 
+    defeats: ["Gemini", "Leo"]
+  }
+];
